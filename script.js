d534709dac870559ecd3fca4bbe2c0ab1525a5cd
Roderigoshq <143416385+Roderigoshq@users.noreply.github.com>
2023-12-04 19:06:48 -0300

    Atualização - All-Navbar/Doações - 04/12 19:06 (2)
    Atualizei o formulário da página de doações, troquei todas as navbar e a página de fotos e projetos está 100% responsivo, porém, devo arrumar ainda o código que está feio de tudo.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
-        document.querySelector('.icon').src = "/TESTE/bars-solid (1).svg"
+        document.querySelector('.icon').src = "/PII_FRONT-END_O_SEMEADOR-main/IMAGENS/bars-solid (1).svg"
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "/TESTE/xmark-solid.svg"
+        document.querySelector('.icon').src = "/PII_FRONT-END_O_SEMEADOR-main/IMAGENS/xmark-solid.svg"
     }
 }
